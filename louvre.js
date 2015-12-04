c5bcee918fafeb3f0d19369f10485e649d05970a
Jens Ohlig <jens.ohlig@wikimedia.de>
2015-12-04 15:49:46 +0100

    Reformat source

--- louvre.js
+++ louvre.js
@@ -1,49 +1,47 @@
 function getArt( callback ) {
-	var wdskim = "https://tools.wmflabs.org/hay/wdskim/index.php";
-	var query = "?prop=P195&item=Q3044768&language=en&withimages=on&format=json";
+    var wdskim = "https://tools.wmflabs.org/hay/wdskim/index.php";
+    var query = "?prop=P195&item=Q3044768&language=en&withimages=on&format=json";
 
-	function createCORSRequest( method, url ) {
-		var xhr = new XMLHttpRequest();
-		if ( 'withCredentials' in xhr ) {
-			// Check if the XMLHttpRequest object has a "withCredentials" property.
-			// "withCredentials" only exists on XMLHTTPRequest2 objects.
-			xhr.open( method, url, true );
-		} else if ( typeof XDomainRequest != "undefined" ) {
-			// Otherwise, check if XDomainRequest.
-			// XDomainRequest only exists in IE, and is IE's way of making CORS requests.
-			xhr = new XDomainRequest();
-			xhr.open( method, url );
-		} else {
-			// Otherwise, CORS is not supported by the browser.
-			xhr = null;
-		}
-		return xhr;
-	}
+    function createCORSRequest( method, url ) {
+        var xhr = new XMLHttpRequest();
+        if ( 'withCredentials' in xhr ) {
+            // Check if the XMLHttpRequest object has a "withCredentials" property.
+            // "withCredentials" only exists on XMLHTTPRequest2 objects.
+            xhr.open( method, url, true );
+        } else if ( typeof XDomainRequest != "undefined" ) {
+            // Otherwise, check if XDomainRequest.
+            // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+            xhr = new XDomainRequest();
+            xhr.open( method, url );
+        } else {
+            // Otherwise, CORS is not supported by the browser.
+            xhr = null;
+        }
+        return xhr;
+    }
 
-	var query_xhr = createCORSRequest( 'GET', wdskim + query );
-	if ( !query_xhr ) {
-		throw new Error( 'CORS not supported' );
-	}
+    var query_xhr = createCORSRequest( 'GET', wdskim + query );
+    if ( !query_xhr ) {
+        throw new Error( 'CORS not supported' );
+    }
 
-	query_xhr.onload = function() {
-		var response = query_xhr.responseText;
-		var items = $.parseJSON( response ).items;
-		$.each( items, function( itemId, item ) {
-			callback( itemId, item );
-		} );
-	};
-	query_xhr.onerror = function() {
-		throw new Error( 'Error making the request' );
-	};
-	query_xhr.send();
+    query_xhr.onload = function() {
+        var response = query_xhr.responseText;
+        var items = $.parseJSON( response ).items;
+        $.each( items, function( itemId, item ) {
+            callback( itemId, item );
+        } );
+    };
+    query_xhr.onerror = function() {
+        throw new Error( 'Error making the request' );
+    };
+    query_xhr.send();
 }
 
 
 function getImageUrl( fileName ) {
-	var fileName = encodeURI(fileName.replace( / /g, '_' ));
-	var hash = md5( fileName );
-	//https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/XXX.jpg/320px-XXX.jpg
-	return 'https://upload.wikimedia.org/wikipedia/commons/thumb/'
-		+ hash.substr( 0, 1 ) + '/' + hash.substr( 0, 2 ) + '/'
-		+ fileName + '/320px-' + fileName;
+    var fileName = encodeURI( fileName.replace( / /g, '_' ) );
+    var hash = md5( fileName );
+    //https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/XXX.jpg/320px-XXX.jpg
+    return 'https://upload.wikimedia.org/wikipedia/commons/thumb/' + hash.substr( 0, 1 ) + '/' + hash.substr( 0, 2 ) + '/' + fileName + '/320px-' + fileName;
 }
--- roguelike.js
+++ roguelike.js
@@ -1,283 +1,303 @@
 var chars = {
-		wall: '#',
-		entity: 'E'
-	},
-	colors = {
-		'@': '#F00',
-		'.': '#630',
-		'#': '#666',
-		'E': '#FF0'
-	};
+        wall: '#',
+        entity: 'E'
+    },
+    colors = {
+        '@': '#F00',
+        '.': '#630',
+        '#': '#666',
+        'E': '#FF0'
+    };
 
 var Game = {
-	display: null,
-	map: {},
-	wikidataEntities: {},
-	engine: null,
-	player: null,
+    display: null,
+    map: {},
+    wikidataEntities: {},
+    engine: null,
+    player: null,
 
-	init: function() {
-		var options = {
-			width: 64,
-			height: 36,
-			forceSquareRatio: true
-		};
-		this.display = new ROT.Display( options );
-		document.getElementById( 'screen' ).appendChild( this.display.getContainer() );
+    init: function() {
+        var options = {
+            width: 64,
+            height: 36,
+            forceSquareRatio: true
+        };
+        this.display = new ROT.Display( options );
+        document.getElementById( 'screen' ).appendChild( this.display.getContainer() );
 
-		this._generateMap();
+        this._generateMap();
 
-		var scheduler = new ROT.Scheduler.Simple();
-		scheduler.add( this.player, true );
+        var scheduler = new ROT.Scheduler.Simple();
+        scheduler.add( this.player, true );
 
-		this.engine = new ROT.Engine( scheduler );
-		this.engine.start();
-	},
+        this.engine = new ROT.Engine( scheduler );
+        this.engine.start();
+    },
 
-	_generateMap: function() {
-		var options = this.display.getOptions();
-		var digger = new ROT.Map.Digger( options.width, options.height - 5 );
-		var freeCells = [];
+    _generateMap: function() {
+        var options = this.display.getOptions();
+        var digger = new ROT.Map.Digger( options.width, options.height - 5 );
+        var freeCells = [];
 
-		var digCallback = function( x, y, value ) {
-			if ( value ) {
-				return;
-			}
+        var digCallback = function( x, y, value ) {
+            if ( value ) {
+                return;
+            }
 
-			var key = x + ',' + y;
-			this.map[key] = '.';
-			freeCells.push( key );
+            var key = x + ',' + y;
+            this.map[ key ] = '.';
+            freeCells.push( key );
 
-			this.createWalls( x, y );
-		};
-		digger.create( digCallback.bind( this ) );
+            this.createWalls( x, y );
+        };
+        digger.create( digCallback.bind( this ) );
 
-		this._generateWikidataEntities( freeCells );
-		this._drawWholeMap();
-		this._createPlayer( freeCells );
-	},
+        this._generateWikidataEntities( freeCells );
+        this._drawWholeMap();
+        this._createPlayer( freeCells );
+        this._placeMonsters( freeCells );
+    },
 
-	createWalls: function( x, y ) {
-		// Surround a walkable cell with a square wall
-		var dirs = ROT.DIRS[8];
-		for ( var i = 0; i < 8; i++ ) {
-			var key = ( x + dirs[i][0] ) + ',' + ( y + dirs[i][1] );
-			if ( !( this.map[key] ) ) {
-				this.map[key] = chars.wall;
-			}
-		}
-	},
+    createWalls: function( x, y ) {
+        // Surround a walkable cell with a square wall
+        var dirs = ROT.DIRS[ 8 ];
+        for ( var i = 0; i < 8; i++ ) {
+            var key = ( x + dirs[ i ][ 0 ] ) + ',' + ( y + dirs[ i ][ 1 ] );
+            if ( !( this.map[ key ] ) ) {
+                this.map[ key ] = chars.wall;
+            }
+        }
+    },
 
-	_createPlayer: function( freeCells ) {
-		var index = Math.floor( ROT.RNG.getUniform() * freeCells.length );
-		var key = freeCells.splice( index, 1 )[0];
-		var parts = key.split( ',' ),
-			x = parts[0] | 0,
-			y = parts[1] | 0;
-		this.player = new Player( x, y );
-	},
+    _createPlayer: function( freeCells ) {
+        var index = Math.floor( ROT.RNG.getUniform() * freeCells.length );
+        var key = freeCells.splice( index, 1 )[ 0 ];
+        var parts = key.split( ',' ),
+            x = parts[ 0 ] | 0,
+            y = parts[ 1 ] | 0;
+        this.player = new Player( x, y );
+    },
 
-	_generateWikidataEntities: function( freeCells ) {
-		var toFill = [];
+    _generateWikidataEntities: function( freeCells ) {
+        var toFill = [];
 
-		for ( var i = 0; i < 20; i++ ) {
-			var index = Math.floor( ROT.RNG.getUniform() * freeCells.length );
-			var key = freeCells.splice( index, 1 )[0];
-			this.wikidataEntities[key] = {
-				title: 'Nothing to see here',
-				beer: Math.random() > 0.7 ? 1 : 0,
-				cake: Math.random() > 0.7 ? 1 : 0
-			};
-			this.map[key] = chars.entity;
-			toFill.push( key );
-		}
+        for ( var i = 0; i < 20; i++ ) {
+            var index = Math.floor( ROT.RNG.getUniform() * freeCells.length );
+            var key = freeCells.splice( index, 1 )[ 0 ];
+            this.wikidataEntities[ key ] = {
+                title: 'Nothing to see here',
+                beer: Math.random() > 0.7 ? 1 : 0,
+                cake: Math.random() > 0.7 ? 1 : 0
+            };
+            this.map[ key ] = chars.entity;
+            toFill.push( key );
+        }
 
-		var self = this;
-		getArt( function( itemId, item ) {
-			var key = toFill.shift();
-			if ( !key ) {
-				return;
-			}
+        var self = this;
+        getArt( function( itemId, item ) {
+            var key = toFill.shift();
+            if ( !key ) {
+                return;
+            }
 
-			self.wikidataEntities[key].title = item.label + ', ' + item.description;
-			self.wikidataEntities[key].image = item.image;
-		} );
-	},
+            self.wikidataEntities[ key ].title = item.label + ', ' + item.description;
+            self.wikidataEntities[ key ].image = item.image;
+        } );
+    },
 
-	isCell: function( x, y, char ) {
-		var key = x + ',' + y;
-		return key in this.map && this.map[key] === char;
-	},
+    _placeAnimals: function( freeCells ) {
+        var toFill = [];
 
-	isFreeCell: function( x, y ) {
-		var key = x + ',' + y;
-		return !( key in this.map );
-	},
+        for ( var i = 0; i < 20; i++ ) {
+            var index = Math.floor( ROT.RNG.getUniform() * freeCells.length );
+            var key = freeCells.splice( index, 1 )[ 0 ];
+            this.wikidataEntities[ key ] = {
+                title: 'Nothing to see here',
+            };
+            this.map[ key ] = chars.entity;
+            toFill.push( key );
+        }
 
-	isWalkableCell: function( x, y ) {
-		var key = x + ',' + y;
-		return key in this.map && this.map[key] !== chars.wall;
-	},
+        var self = this;
+        getArt( function( itemId, item ) {
+            var key = toFill.shift();
+            if ( !key ) {
+                return;
+            }
 
-	drawCell: function( key ) {
-		var parts = key.split( ',' ),
-			x = parts[0] | 0,
-			y = parts[1] | 0,
-			char = this.map[key];
-		this.display.draw( x, y, char, colors[char] || '#CCC' );
-	},
+            self.wikidataEntities[ key ].title = item.label + ', ' + item.description;
+            self.wikidataEntities[ key ].image = item.image;
+        } );
+    },
 
-	drawTextBox: function( text ) {
-		var options = this.display.getOptions(),
-			player = this.player || {};
+    isCell: function( x, y, char ) {
+        var key = x + ',' + y;
+        return key in this.map && this.map[ key ] === char;
+    },
 
-		for ( var y = options.height - 1; y > options.height - 5; y-- ) {
-			this.display.drawText( 0, y, '%c{#333}' + Array( options.width + 1 ).join( '\\' ) );
-		}
-		this.display.drawText( 0, y, '%c{#FFF}' + Array( options.width + 1 ).join( '~' ) );
-		this.display.drawText( 0, options.height - 4, '%c{#F00}' + ( text || '' ) );
-		var status = 'Beer: ' + ( player.beer || 0 ) + '  Cake: ' + ( player.cake || 0 );
-		this.display.drawText( options.width - status.length, options.height - 1, '%c{#090}' + status );
-	},
+    isFreeCell: function( x, y ) {
+        var key = x + ',' + y;
+        return !( key in this.map );
+    },
 
-	_drawWholeMap: function() {
-		for ( var key in this.map ) {
-			this.drawCell( key );
-		}
+    isWalkableCell: function( x, y ) {
+        var key = x + ',' + y;
+        return key in this.map && this.map[ key ] !== chars.wall;
+    },
 
-		this.drawTextBox();
-	}
+    drawCell: function( key ) {
+        var parts = key.split( ',' ),
+            x = parts[ 0 ] | 0,
+            y = parts[ 1 ] | 0,
+            char = this.map[ key ];
+        this.display.draw( x, y, char, colors[ char ] || '#CCC' );
+    },
+
+    drawTextBox: function( text ) {
+        var options = this.display.getOptions(),
+            player = this.player || {};
+
+        for ( var y = options.height - 1; y > options.height - 5; y-- ) {
+            this.display.drawText( 0, y, '%c{#333}' + Array( options.width + 1 ).join( '\\' ) );
+        }
+        this.display.drawText( 0, y, '%c{#FFF}' + Array( options.width + 1 ).join( '~' ) );
+        this.display.drawText( 0, options.height - 4, '%c{#F00}' + ( text || '' ) );
+        var status = 'Beer: ' + ( player.beer || 0 ) + '  Cake: ' + ( player.cake || 0 );
+        this.display.drawText( options.width - status.length, options.height - 1, '%c{#090}' + status );
+    },
+
+    _drawWholeMap: function() {
+        for ( var key in this.map ) {
+            this.drawCell( key );
+        }
+
+        this.drawTextBox();
+    }
 };
 
 var Player = function( x, y ) {
-	this._x = x;
-	this._y = y;
-	this.beer = 0;
-	this.cake = 0;
-	this._draw();
+    this._x = x;
+    this._y = y;
+    this.beer = 0;
+    this.cake = 0;
+    this._draw();
 };
 
 Player.prototype.act = function() {
-	Game.engine.lock();
-	window.addEventListener( 'keydown', this );
+    Game.engine.lock();
+    window.addEventListener( 'keydown', this );
 };
 
 Player.prototype.handleEvent = function( e ) {
-	var walkingKeyMap = {
-		// Arrow keys, Pos1/End, PgUp/Down
-		38: 0,
-		33: 1,
-		39: 2,
-		34: 3,
-		40: 4,
-		35: 5,
-		37: 6,
-		36: 7,
-		// HJKL
-		72: 6,
-		74: 4,
-		75: 0,
-		76: 2,
-		// WASD
-		87: 0,
-		65: 6,
-		83: 4,
-		68: 2
-	};
+    var walkingKeyMap = {
+        // Arrow keys, Pos1/End, PgUp/Down
+        38: 0,
+        33: 1,
+        39: 2,
+        34: 3,
+        40: 4,
+        35: 5,
+        37: 6,
+        36: 7,
+        // HJKL
+        72: 6,
+        74: 4,
+        75: 0,
+        76: 2,
+        // WASD
+        87: 0,
+        65: 6,
+        83: 4,
+        68: 2
+    };
 
-	var code = e.keyCode,
-		key = this._x + ',' + this._y,
-		options = Game.display.getOptions();
+    var code = e.keyCode,
+        key = this._x + ',' + this._y,
+        options = Game.display.getOptions();
 
-	if ( code in walkingKeyMap ) {
-		var dir = ROT.DIRS[8][walkingKeyMap[code]];
-		var newX = this._x + dir[0];
-		var newY = this._y + dir[1];
-		if ( !Game.isWalkableCell( newX, newY ) ) {
-			return;
-		}
+    if ( code in walkingKeyMap ) {
+        var dir = ROT.DIRS[ 8 ][ walkingKeyMap[ code ] ];
+        var newX = this._x + dir[ 0 ];
+        var newY = this._y + dir[ 1 ];
+        if ( !Game.isWalkableCell( newX, newY ) ) {
+            return;
+        }
 
-		Game.drawCell( key );
-		this._x = newX;
-		this._y = newY;
-		this._draw();
-		Game.drawTextBox();
-	} else if ( code === ROT.VK_B
-		&& this.beer
-	) {
-		this.beer--;
-		var size = 3;
-		for ( var deltaX = -size; deltaX <= size; deltaX++ ) {
-			for ( var deltaY = -size; deltaY <= size; deltaY++ ) {
-				// Star shape
-				if ( Math.abs( deltaX ) + Math.abs( deltaY ) > 3 ) {
-					continue;
-				}
+        Game.drawCell( key );
+        this._x = newX;
+        this._y = newY;
+        this._draw();
+        Game.drawTextBox();
+    } else if ( code === ROT.VK_B && this.beer ) {
+        this.beer--;
+        var size = 3;
+        for ( var deltaX = -size; deltaX <= size; deltaX++ ) {
+            for ( var deltaY = -size; deltaY <= size; deltaY++ ) {
+                // Star shape
+                if ( Math.abs( deltaX ) + Math.abs( deltaY ) > 3 ) {
+                    continue;
+                }
 
-				var x = this._x + deltaX,
-					y = this._y + deltaY;
-				if ( x <= 0 || y <= 0 || x >= options.width - 1 || y >= options.height - 6 ) {
-					continue;
-				}
+                var x = this._x + deltaX,
+                    y = this._y + deltaY;
+                if ( x <= 0 || y <= 0 || x >= options.width - 1 || y >= options.height - 6 ) {
+                    continue;
+                }
 
-				if ( !Game.isFreeCell( x, y ) && !Game.isCell( x, y, chars.wall ) ) {
-					continue;
-				}
+                if ( !Game.isFreeCell( x, y ) && !Game.isCell( x, y, chars.wall ) ) {
+                    continue;
+                }
 
-				Game.map[x + ',' + y] = '.';
-				Game.createWalls( x, y );
-				Game._drawWholeMap();
-				this._draw();
-			}
-		}
-		Game.drawTextBox( 'You took a nip, and BOOM!' );
-	} else if ( ( code === ROT.VK_SPACE || code === ROT.VK_E )
-		//&& Game.isCell( this._x, this._y, chars.entity )
-		&& Game.wikidataEntities[key]
-	) {
-		var wikidataEntity = Game.wikidataEntities[key] || {},
-			title = wikidataEntity.title;
+                Game.map[ x + ',' + y ] = '.';
+                Game.createWalls( x, y );
+                Game._drawWholeMap();
+                this._draw();
+            }
+        }
+        Game.drawTextBox( 'You took a nip, and BOOM!' );
+    } else if ( ( code === ROT.VK_SPACE || code === ROT.VK_E )
+        //&& Game.isCell( this._x, this._y, chars.entity )
+        && Game.wikidataEntities[ key ]
+    ) {
+        var wikidataEntity = Game.wikidataEntities[ key ] || {},
+            title = wikidataEntity.title;
 
-		// Loot the entity
-		Game.map[key] = 'e';
-		var beer = wikidataEntity.beer || 0,
-			cake = wikidataEntity.cake || 0;
-		this.beer += beer;
-		this.cake += cake;
+        // Loot the entity
+        Game.map[ key ] = 'e';
+        var beer = wikidataEntity.beer || 0,
+            cake = wikidataEntity.cake || 0;
+        this.beer += beer;
+        this.cake += cake;
 
-		var msg = title
-			? 'You looted this entity: ' + title
-			: 'This entity is empty :-(';
-		if ( beer ) {
-			msg += '\nYou found ' + ( beer === 1 ? 'a' : beer ) + ' beer (press B)';
-      beer = 0;
-		}
-		if ( cake ) {
-			msg += '\nYou found ' + ( cake === 1 ? 'a cake' : cake + ' cakes' );
-      cake = 0;
-		}
-		Game.drawTextBox( msg );
-		//alert(wikidataEntity.image);
-		var url = getImageUrl( wikidataEntity.image );
-		//alert(url);
-		document.getElementById( 'painting' ).src = url;
+        var msg = title ? 'You looted this entity: ' + title : 'This entity is empty :-(';
+        if ( beer ) {
+            msg += '\nYou found ' + ( beer === 1 ? 'a' : beer ) + ' beer (press B)';
+            beer = 0;
+        }
+        if ( cake ) {
+            msg += '\nYou found ' + ( cake === 1 ? 'a cake' : cake + ' cakes' );
+            cake = 0;
+        }
+        Game.drawTextBox( msg );
+        var url = getImageUrl( wikidataEntity.image );
+        document.getElementById( 'painting' ).src = url;
 
-		// Empty the entity
-		wikidataEntity.title = '';
-		wikidataEntity.beer = 0;
-		wikidataEntity.cake = 0;
-	} else {
-		// Debug only: show key code in title.
-		document.title = code;
-		return;
-	}
+        // Empty the entity
+        wikidataEntity.title = '';
+        wikidataEntity.beer = 0;
+        wikidataEntity.cake = 0;
+    } else {
+        // Debug only: show key code in title.
+        document.title = code;
+        return;
+    }
 
-	window.removeEventListener( 'keydown', this );
-	Game.engine.unlock();
+    window.removeEventListener( 'keydown', this );
+    Game.engine.unlock();
 };
 
 Player.prototype._draw = function() {
-	Game.display.draw( this._x, this._y, '@', colors['@'] );
+    Game.display.draw( this._x, this._y, '@', colors[ '@' ] );
 };
 
 Game.init();
