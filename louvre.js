41833ef9d946143c8b4a80b2ba99abbca162d0b8
Jens Ohlig <jens.ohlig@wikimedia.de>
2016-09-04 17:57:26 +0200

    Rewrite as AJAX call.

--- louvre.js
+++ louvre.js
@@ -1,41 +1,22 @@
 function getArt( callback ) {
-    var wdskim = "https://tools.wmflabs.org/hay/wdskim/index.php";
-    var query = "?prop=P195&item=Q3044768&language=en&withimages=on&format=json";
-
-    function createCORSRequest( method, url ) {
-        var xhr = new XMLHttpRequest();
-        if ( 'withCredentials' in xhr ) {
-            // Check if the XMLHttpRequest object has a "withCredentials" property.
-            // "withCredentials" only exists on XMLHTTPRequest2 objects.
-            xhr.open( method, url, true );
-        } else if ( typeof XDomainRequest != "undefined" ) {
-            // Otherwise, check if XDomainRequest.
-            // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
-            xhr = new XDomainRequest();
-            xhr.open( method, url );
-        } else {
-            // Otherwise, CORS is not supported by the browser.
-            xhr = null;
+    $.ajax({
+        url: "https://tools.wmflabs.org/hay/wdskim/index.php",
+        data: {
+          format: "json",
+          action: "query",
+          language: "en",
+          prop:"P195",
+          item: "Q3044768",
+          withimages: "on"
+        },
+//        dataType: 'jsonp',
+        success: function (data) {
+            var items = data.items;
+            $.each( items, function( itemId, item ) {
+                callback( itemId, item );
+            } );
         }
-        return xhr;
-    }
-
-    var query_xhr = createCORSRequest( 'GET', wdskim + query );
-    if ( !query_xhr ) {
-        throw new Error( 'CORS not supported' );
-    }
-
-    query_xhr.onload = function() {
-        var response = query_xhr.responseText;
-        var items = $.parseJSON( response ).items;
-        $.each( items, function( itemId, item ) {
-            callback( itemId, item );
-        } );
-    };
-    query_xhr.onerror = function() {
-        throw new Error( 'Error making the request' );
-    };
-    query_xhr.send();
+    });
 }
 
 
