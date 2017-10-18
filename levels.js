ce53262699fd6c16f09de8da0365bc58e42313bf
Jens Ohlig <jens.ohlig@wikimedia.de>
2017-10-18 13:36:30 +0200

    Add two more levels

--- levels.js
+++ levels.js
@@ -30,7 +30,7 @@ var levels =
       SELECT ?item ?label ?image ?description
       WHERE
       {
-        ?item ?x1 wd:Q146 . 
+        ?item ?x1 wd:Q146 .
         OPTIONAL {
           ?item wdt:P18 ?image
         }
@@ -42,5 +42,35 @@ var levels =
       }
     `,
     description: "Cat things"
+  },
+  {
+    sparql: `
+      SELECT ?item ?label ?description ?image
+      WHERE
+      {
+        ?item wdt:P180 wd:Q41213 .
+        OPTIONAL {?item wdt:P18 ?image . }
+        ?item rdfs:label ?label .
+        ?item schema:description ?description .
+        filter (lang(?label) = "en") .
+        filter (lang(?description) = "en") .
+      }
+      `,
+    description: "Towers of Babel"
+  },
+  {
+    sparql: `
+      SELECT ?item ?label ?description ?image
+      WHERE
+      {
+        ?item wdt:P31 wd:Q26529 .
+        ?item rdfs:label ?label .
+        ?item schema:description ?description .
+        filter (lang(?label) = "en") .
+        filter (lang(?description) = "en") .
+        OPTIONAL { ?item wdt:P18 ?image }
+      }
+      `,
+    description: "Space probes"
   }
 ]
